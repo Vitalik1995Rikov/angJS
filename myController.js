5c82f07bbc8a512bdda3dd13659905235321babf
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-06 10:42:16 +0300

    DOM

--- myController.js
+++ myController.js
@@ -1,7 +1,3 @@
 myApp.controller('myController', function($scope) {
-    $scope.names = {
-        person1: {name: 'John', lastname: 'Smith'},
-        person2: {name: 'Bob', lastname: 'Azerman'},
-        person3: {name: 'Lui', lastname: 'Fletcher'}
-    }
+    
 });
--- readme.md
+++ readme.md
@@ -14,3 +14,4 @@
 + http
 + tables
 + select boxes
++ DOM
